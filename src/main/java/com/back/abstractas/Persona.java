105ab46cb0332e61a91379332b143f235decc69f
Andres Padilla <jandrespadilla@gmail.com>
2024-10-24 18:45:23 -0300

    Subo interface Persona

---
+++ src/main/java/com/back/abstractas/Persona.java
@@ -0,0 +1,50 @@
+package com.back.abstractas;
+
+import com.back.interfaces.Identificable;
+
+public abstract class Persona implements Identificable {
+    protected String nombre;
+    protected String apellido;
+    protected String dni;
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setId(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+    public Persona(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
