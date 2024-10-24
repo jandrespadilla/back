eb4f3cdb54e092444a871ec623d017c8cf523b95
Andres Padilla <jandrespadilla@gmail.com>
2024-10-24 17:31:01 -0300

    primera subida

---
+++ src/test/java/com/negocio/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negocio.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
